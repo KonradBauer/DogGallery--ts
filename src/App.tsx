d673c663bd98a854606546a63b0978b0564ed3a1
Konrad Bauer <konradbauer94@gmail.com>
2023-11-06 00:50:37 +0100

    added template bg color and font color using tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import GetData from "./API/tanstack";
 
 const App: React.FC = () => {
   return (
-    <div>
+    <div className="py-3 px-3 flex justify-center bg-teal-300 text-slate-500 border-spacing-0">
       <GetData />
     </div>
   );
