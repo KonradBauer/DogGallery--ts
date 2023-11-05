00156be68dfe34bf792aae8fa9b6a5e8680a06a1
Konrad Bauer <konradbauer94@gmail.com>
2023-11-06 00:51:02 +0100

    create loader animation, add 1 sec delay to see loader

--- src/API/tanstack.tsx
+++ src/API/tanstack.tsx
@@ -3,10 +3,20 @@ import { useQuery } from "@tanstack/react-query";
 import { fetchData } from "./getAPI";
 
 const GetData: React.FC = () => {
-  const { isLoading, error, data } = useQuery({ queryKey: ["breeds"], queryFn: fetchData });
+  const { isLoading, error, data } = useQuery({
+    queryKey: ["breeds"],
+    queryFn: async () => {
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+      return fetchData();
+    },
+  });
 
   if (isLoading) {
-    return <div>Ładowanie...</div>;
+    return (
+      <div className="flex h-screen items-center justify-center">
+        <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-blue-500"></div>
+      </div>
+    );
   }
 
   if (error) {
