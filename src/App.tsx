cba418613ecb1da9486f44475c025c876821d5c7
Konrad Bauer <konradbauer94@gmail.com>
2023-11-06 00:09:34 +0100

    App is now render file

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,10 @@
 import React from "react";
-import axios from "axios";
-import { useQuery } from "@tanstack/react-query";
-
-interface Dog {
-  name: string;
-}
+import GetData from "./API/tanstack";
 
 const App: React.FC = () => {
-  const fetchDogs = async () => {
-    try {
-      const name = "doberman";
-      const response = await axios.get(`https://api.api-ninjas.com/v1/dogs?name=${name}`, {
-        headers: {
-          "X-Api-Key": "o7wqm2xxGZlYQ0dUeKoIuQ==PRLizpPUVv2xPXkd",
-        },
-      });
-
-      const { breeds } = response.data.message;
-      const dogList: Dog[] = Object.keys(breeds).map((breedName) => ({
-        name: breedName,
-      }));
-      return dogList;
-    } catch (error) {
-      throw new Error("Network response was not ok");
-    }
-  };
-
-  const { isPending, error, data } = useQuery<Dog[]>({
-    queryKey: ["dogs"],
-    queryFn: fetchDogs,
-  });
-
-  if (isPending) {
-    return <div>Ładowanie...</div>;
-  }
-
-  if (error) {
-    return <div>Błąd: {error.message}</div>;
-  }
-
   return (
     <div>
-      {data?.map((dog) => (
-        <div key={dog.name}>{dog.name}</div>
-      ))}
+      <GetData />
     </div>
   );
 };
