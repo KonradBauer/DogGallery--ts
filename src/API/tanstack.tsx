ccac6739337ee0e8fd1d594318f8440756f7fcfe
Konrad Bauer <konradbauer94@gmail.com>
2023-11-06 00:54:55 +0100

    modify loader styles

--- src/API/tanstack.tsx
+++ src/API/tanstack.tsx
@@ -13,8 +13,8 @@ const GetData: React.FC = () => {
 
   if (isLoading) {
     return (
-      <div className="flex h-screen items-center justify-center">
-        <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-blue-500"></div>
+      <div className="flex justify-center items-center h-screen">
+        <div className="loader ease-linear rounded-full border-t-5 border-r-1 border-b-4 border-gray-500 h-12 w-12 animate-spin"></div>
       </div>
     );
   }
