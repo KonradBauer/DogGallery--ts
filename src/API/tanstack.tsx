e3316674e9f43de676ec619af308053bb410e969
Konrad Bauer <konradbauer94@gmail.com>
2023-11-07 00:01:01 +0100

    type all API keys

--- src/API/tanstack.tsx
+++ src/API/tanstack.tsx
@@ -5,6 +5,19 @@ import { fetchData } from "./getAPI";
 interface Breed {
   id: string;
   url: string;
+  name: string;
+  breed_group: string;
+  bred_for: string;
+  height: {
+    imperial: string;
+    metric: string;
+  };
+  weight: {
+    imperial: string;
+    metric: string;
+  };
+  temperament: string;
+  life_span: string;
 }
 
 const GetData: React.FC = () => {
@@ -33,10 +46,47 @@ const GetData: React.FC = () => {
   }
 
   return (
-    <ul>
+    <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
       {data.map((breed) => (
-        <li key={breed.id}>
-          <img src={breed.url} alt={breed.id} width="200" height="150" />
+        <li key={breed.id} className="bg-white rounded-lg shadow-md p-4">
+          <img
+            src={breed.url}
+            alt={breed.id}
+            className="w-full h-40 object-cover mb-4 rounded-md"
+          />
+          <h3 className="text-xl font-semibold mb-2">{breed.name}</h3>
+          <div className="text-center">
+            {breed.breed_group && (
+              <p className="mb-2">
+                <strong>Breed Group:</strong> {breed.breed_group}
+              </p>
+            )}
+            {breed.bred_for && (
+              <p className="mb-2">
+                <strong>Bred For:</strong> {breed.bred_for}
+              </p>
+            )}
+            {breed.height && breed.height.imperial && breed.height.metric && (
+              <p className="mb-2">
+                <strong>Height:</strong> {breed.height.imperial} inches ({breed.height.metric} cm)
+              </p>
+            )}
+            {breed.weight && breed.weight.imperial && breed.weight.metric && (
+              <p className="mb-2">
+                <strong>Weight:</strong> {breed.weight.imperial} pounds ({breed.weight.metric} kg)
+              </p>
+            )}
+            {breed.temperament && (
+              <p className="mb-2">
+                <strong>Temperament:</strong> {breed.temperament}
+              </p>
+            )}
+            {breed.life_span && (
+              <p>
+                <strong>Life Span:</strong> {breed.life_span}
+              </p>
+            )}
+          </div>
         </li>
       ))}
     </ul>
