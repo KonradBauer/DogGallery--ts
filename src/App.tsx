56c4de35cac87c1ec7ec50c360330cd182722037
Konrad Bauer <konradbauer94@gmail.com>
2023-11-06 01:04:57 +0100

    added bg gradient

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import GetData from "./API/tanstack";
 
 const App: React.FC = () => {
   return (
-    <div className="py-3 px-3 flex justify-center bg-teal-300 text-slate-500 border-spacing-0">
+    <div className="py-3 px-3 flex justify-center bg-teal-300 text-slate-500 border-spacing-0 bg-gradient-to-br from-teal-400 via-teal-500 to-teal-600">
       <GetData />
     </div>
   );
