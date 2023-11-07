50b5793f2a61b236ebbdece1e4396c132f2c6ac9
Konrad Bauer <konradbauer94@gmail.com>
2023-11-08 00:41:26 +0100

    rebuild navbar, add logo - this logo is a dropdown button

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,18 +1,36 @@
 import React from "react";
+import logo from "../../images/logo.png";
 
 export const Navbar: React.FC = () => {
   return (
-    <div className="navbar bg-base-300">
+    <div className="navbar bg-base-100">
       <div className="flex-1">
-        <a href="/#" className="btn btn-ghost normal-case text-sm md:text-lg lg:text-xl">
-          The Dogs Gallery
+        <a href="/" className="btn btn-ghost normal-case text-xl">
+          Dogs Gallery
         </a>
       </div>
       <div className="flex-none gap-2">
         <div className="form-control">
-          <input type="text" placeholder="Szukaj" className="input input-bordered w-24 md:w-auto" />
+          <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto" />
         </div>
-        <div className="dropdown dropdown-end"></div>
+        <div className="dropdown dropdown-end">
+          <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+            <div className="w-10 rounded-full">
+              <img src={logo} alt="app logo" />
+            </div>
+          </label>
+          <ul
+            tabIndex={0}
+            className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52"
+          >
+            <li>
+              <a href="/" className="justify-between">
+                Dogs breeds
+                <span className="badge">Check!</span>
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
