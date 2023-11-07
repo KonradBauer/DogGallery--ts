243bc5daab3ae95fb05bef12b1fc5ca66dc58849
Konrad Bauer <konradbauer94@gmail.com>
2023-11-08 00:40:43 +0100

    change app bg

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ const App: React.FC = () => {
   return (
     <>
       <Navbar />
-      <div className="py-3 px-3 flex justify-center bg-teal-300 text-indigo-800 font-open-sans font-semibold border-spacing-0 bg-gradient-to-br from-teal-400 via-teal-500 to-teal-600">
+      <div className="py-3 px-3 flex justify-center bg-neutral text-indigo-800 font-open-sans font-semibold border-spacing-0 ">
         <GetData />
       </div>
       <Footer />
