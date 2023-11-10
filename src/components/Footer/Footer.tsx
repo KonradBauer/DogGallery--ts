d3fe957495a30304c2d6ca0d0e5e64427367a1c1
Konrad Bauer <konradbauer94@gmail.com>
2023-11-11 00:03:07 +0100

    modify footer link, add target blank, rel= noopener noreferrer, move address and name to separate const

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,11 +1,14 @@
 export const Footer = () => {
+  const address = "https://github.com/KonradBauer";
+  const name = "Konrad Bauer";
+
   return (
     <footer className="footer footer-center p-4 bg-base-300 text-base-content">
       <aside>
         <p>
           Copyright © 2023 - All right reserved by{" "}
-          <a href="https://github.com/KonradBauer">
-            <span className="font-semibold link-hover">Konrad Bauer</span>
+          <a href={address} target="_blank" rel="noopener noreferrer">
+            <span className="font-semibold link-hover">{name}</span>
           </a>
         </p>
       </aside>
