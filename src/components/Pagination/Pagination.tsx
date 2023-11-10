3f74c68979f84409b682c701c33634f5a19800fe
Konrad Bauer <konradbauer94@gmail.com>
2023-11-10 23:40:45 +0100

    add handlers to Pagination, not working yet, but visually working

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,9 +1,25 @@
+import { useState } from "react";
+
 export const Pagination = () => {
+  const [page, setPage] = useState(1);
+
+  const handleNextPage = () => {
+    setPage((prevPage) => prevPage + 1);
+  };
+
+  const handlePrevPage = () => {
+    setPage((prevPage) => Math.max(prevPage - 1, 1));
+  };
+
   return (
     <div className="join bg-neutral flex justify-center py-2 rounded-none">
-      <button className="join-item btn">«</button>
-      <button className="join-item btn">Page 1</button>
-      <button className="join-item btn">»</button>
+      <button onClick={handlePrevPage} disabled={page === 1} className="join-item btn">
+        «
+      </button>
+      <button className="join-item btn">Page {page}</button>
+      <button onClick={handleNextPage} className="join-item btn">
+        »
+      </button>
     </div>
   );
 };
