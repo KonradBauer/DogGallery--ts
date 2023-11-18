c23018be8e5cb13e2248415095f21a870ef833c7
Konrad Bauer <konradbauer94@gmail.com>
2023-11-18 15:39:00 +0100

    move Navbar out of the Homepage component

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,23 @@ import React from "react";
 import { Route, Switch, HashRouter } from "react-router-dom";
 import { Homepage } from "./components/Homepage/homepage";
 import { DogDetails } from "./components/DogDetails/DogDetails";
+import { Navbar } from "./components/Navbar/Navbar";
 
 const App: React.FC = () => {
   return (
-    <HashRouter>
-      <Switch>
-        <Route path="/dog-details/:id">
-          <DogDetails />
-        </Route>
-        <Route path="/">
-          <Homepage />
-        </Route>
-      </Switch>
-    </HashRouter>
+    <>
+      <HashRouter>
+        <Navbar />
+        <Switch>
+          <Route path="/dog-details/:id">
+            <DogDetails />
+          </Route>
+          <Route path="/">
+            <Homepage />
+          </Route>
+        </Switch>
+      </HashRouter>
+    </>
   );
 };
 
--- src/components/Homepage/homepage.tsx
+++ src/components/Homepage/homepage.tsx
@@ -1,6 +1,3 @@
-import React from "react";
-
-import { Navbar } from "../Navbar/Navbar";
 import { Footer } from "../Footer/Footer";
 import { GetBreeds } from "../../Renderers/GetBreeds";
 import { MainContainer } from "../MainContainer/MainContainer";
@@ -8,7 +5,6 @@ import { MainContainer } from "../MainContainer/MainContainer";
 export const Homepage: React.FC = () => {
   return (
     <>
-      <Navbar />
       <MainContainer>
         <GetBreeds />
       </MainContainer>
