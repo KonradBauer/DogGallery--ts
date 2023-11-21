623b8d16bab574006a62a9a5d3b269590179ae11
Konrad Bauer <konradbauer94@gmail.com>
2023-11-21 22:05:08 +0100

    removed unnecessary div

--- src/components/DogDetails/DogDetails.tsx
+++ src/components/DogDetails/DogDetails.tsx
@@ -1,10 +1,5 @@
 import { GetDogDetails } from "../../Views/GetDogDetails";
 
 export const DogDetails: React.FC = () => {
-  return (
-    <div>
-      <GetDogDetails />
-      <div className="w-full fixed bottom-0"></div>
-    </div>
-  );
+  return <GetDogDetails />;
 };
