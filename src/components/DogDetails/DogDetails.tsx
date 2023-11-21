cd195c6c4b1a200689b4fbdb475435e02a55fdfe
Konrad Bauer <konradbauer94@gmail.com>
2023-11-21 21:59:32 +0100

    hide footer in DogDetails

--- src/components/DogDetails/DogDetails.tsx
+++ src/components/DogDetails/DogDetails.tsx
@@ -1,13 +1,10 @@
 import { GetDogDetails } from "../../Views/GetDogDetails";
-import { Footer } from "../Footer/Footer";
 
 export const DogDetails: React.FC = () => {
   return (
     <div>
       <GetDogDetails />
-      <div className="w-full fixed bottom-0">
-        <Footer />
-      </div>
+      <div className="w-full fixed bottom-0"></div>
     </div>
   );
 };
