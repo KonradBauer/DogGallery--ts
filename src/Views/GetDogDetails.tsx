69414cd2e8c050a6338d8a2a58f960a268e45f89
Konrad Bauer <konradbauer94@gmail.com>
2023-11-21 23:30:49 +0100

    change dogDetails content layout

--- src/Views/GetDogDetails.tsx
+++ src/Views/GetDogDetails.tsx
@@ -64,50 +64,53 @@ export const GetDogDetails: React.FC = () => {
   } = data;
 
   return (
-    <div className="bg-white shadow-md p-4 text-neutral h-screen bg-gradient-to-b from-white to-neutral-500">
-      <h1 className="text-xl font-bold mb-4 flex justify-center">{name}</h1>
-      <img
-        src={`https://cdn2.thedogapi.com/images/${reference_image_id}.jpg`}
-        alt={reference_image_id}
-        className="w-full h-60 object-contain mb-4"
-      />
-
-      <div className="text-center">
-        {breed_group && (
-          <p className="mb-2">
-            <strong>Breed Group:</strong> {breed_group}
+    <div className="bg-white shadow-md p-4 text-neutral bg-gradient-to-b from-white to-neutral-500 flex justify-center items-center">
+      <div className="hero-content flex-col lg:flex-row ">
+        <img
+          src={`https://cdn2.thedogapi.com/images/${reference_image_id}.jpg`}
+          alt={reference_image_id}
+          className="max-w-sm w-full rounded-lg shadow-2xl"
+        />
+        <div className="flex justify-start">
+          <p className="py-6">
+            <h1 className="text-4xl font-bold mb-10">{name}</h1>
+            {breed_group && (
+              <p className="mb-2">
+                <strong>Breed Group:</strong> {breed_group}
+                {bred_for && (
+                  <p className="mb-2">
+                    <strong>Bred For:</strong> {bred_for}
+                  </p>
+                )}
+                {height && height.imperial && height.metric && (
+                  <p className="mb-2">
+                    <strong>Height:</strong> {height.imperial} inches ({height.metric} cm)
+                  </p>
+                )}
+                {weight && weight.imperial && weight.metric && (
+                  <p className="mb-2">
+                    <strong>Weight:</strong> {weight.imperial} pounds ({weight.metric} kg)
+                  </p>
+                )}
+                {temperament && (
+                  <p className="mb-2">
+                    <strong>Temperament:</strong> {temperament}
+                  </p>
+                )}
+                {life_span && (
+                  <p className="mb-2">
+                    <strong>Life Span:</strong> {life_span}
+                  </p>
+                )}
+                {origin && (
+                  <p className="mb-2">
+                    <strong>Origin:</strong> {origin}
+                  </p>
+                )}
+              </p>
+            )}
           </p>
-        )}
-        {bred_for && (
-          <p className="mb-2">
-            <strong>Bred For:</strong> {bred_for}
-          </p>
-        )}
-        {height && height.imperial && height.metric && (
-          <p className="mb-2">
-            <strong>Height:</strong> {height.imperial} inches ({height.metric} cm)
-          </p>
-        )}
-        {weight && weight.imperial && weight.metric && (
-          <p className="mb-2">
-            <strong>Weight:</strong> {weight.imperial} pounds ({weight.metric} kg)
-          </p>
-        )}
-        {temperament && (
-          <p className="mb-2">
-            <strong>Temperament:</strong> {temperament}
-          </p>
-        )}
-        {life_span && (
-          <p className="mb-2">
-            <strong>Life Span:</strong> {life_span}
-          </p>
-        )}
-        {origin && (
-          <p className="mb-2">
-            <strong>Origin:</strong> {origin}
-          </p>
-        )}
+        </div>
       </div>
     </div>
   );
