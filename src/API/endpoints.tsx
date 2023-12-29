47e0e0d1bfa85d2d26a24f2918456aa109805085
Konrad Bauer <konradbauer94@gmail.com>
2023-12-29 18:03:52 +0100

    create .env to keep here API key

--- src/API/endpoints.tsx
+++ src/API/endpoints.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const headers = {
-  "x-api-key": "live_U4QRaqcAGPAwGk1PTkUZfLarqupGb9KwZxQ3kuxZRL3lIJL8971UwLkXWFTHEdSP",
+  "x-api-key": process.env.REACT_APP_API_KEY,
 };
 
 export const breedsData = async (pageNumber: number, pageSize: number) => {
@@ -10,11 +10,13 @@ export const breedsData = async (pageNumber: number, pageSize: number) => {
       `https://api.thedogapi.com/v1/breeds?page=${pageNumber}&limit=${pageSize}`,
       {
         headers,
-      }
+      },
     );
     return response.data;
   } catch (error) {
-    throw new Error("Failed to download data. Check your internet connection..");
+    throw new Error(
+      "Failed to download data. Check your internet connection..",
+    );
   }
 };
 
@@ -25,13 +27,18 @@ export const searchBreedsData = async () => {
     });
     return response.data;
   } catch (error) {
-    throw new Error("Failed to download data. Check your internet connection..");
+    throw new Error(
+      "Failed to download data. Check your internet connection..",
+    );
   }
 };
 
 export const dogData = async (id: string) => {
   try {
-    const response = await axios.get(`https://api.thedogapi.com/v1/breeds/${id}`, { headers });
+    const response = await axios.get(
+      `https://api.thedogapi.com/v1/breeds/${id}`,
+      { headers },
+    );
     return response.data;
   } catch (error) {
     throw new Error(`Failed to retrieve dog ID data ${id}: ${error}`);
