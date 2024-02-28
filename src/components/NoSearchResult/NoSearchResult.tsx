d9b265fe4ed849d966ca0c9727538972d1b6f8fa
Konrad Bauer <konradbauer94@gmail.com>
2024-02-28 16:35:42 +0100

    change message to user

--- src/components/NoSearchResult/NoSearchResult.tsx
+++ src/components/NoSearchResult/NoSearchResult.tsx
@@ -4,7 +4,9 @@ export const NoSearchResult = () => {
   return (
     <>
       <div className="flex-row justify-center align-middle">
-        <div className="text-white text-xl text-center">No search results</div>
+        <div className="text-center text-xl text-white">
+          No search results :(
+        </div>
         <img src={noResult} alt="no result" />
       </div>
     </>
